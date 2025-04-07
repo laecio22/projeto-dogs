3b39a6f31678c1e9374056eb4490510323108c34
laecio22 <laecon22@hotmail.com>
2025-04-07 11:12:34 -0300

    feat:implementação  funcionalidade  de  scroll  infinito

--- projetoDogs/src/components/Feed/FeedPhotos/FeedPhotos.tsx
+++ projetoDogs/src/components/Feed/FeedPhotos/FeedPhotos.tsx
@@ -6,18 +6,19 @@ import Error from "../../../helper/Error.js"
 import Loading from "../../../helper/Loading.js"
 import { ContainerListPhotos } from "./style.js"
 
-const FeedPhotos = ({setModalPhoto}) => {
+const FeedPhotos = ({setModalPhoto, user, page, setInfinite}) => {
    const {data, loading, error, request} = useRequest()
 
    useEffect(()=>{
      const fetchPhotos = async() => {
-        const {url, options} = GET_PHOTOS({page:1, total:6, user:0})
-       const {json} = await request(url, options)
-       console.log(json)
+        const {url, options} = GET_PHOTOS({page, total:3, user})
+       const {response,json} = await request(url, options)
+       console.log('requset',json)
+       if(response && response.ok && json.length < 3) setInfinite(false)
      }
 
      fetchPhotos()
-   },[request])
+   },[request, user,page])
   if (error) return <Error error={error}/>
   if (loading) return <Loading/>
   if (data)
--- projetoDogs/src/components/User/User.tsx
+++ projetoDogs/src/components/User/User.tsx
@@ -3,14 +3,17 @@ import { Route, Routes } from "react-router-dom"
 import { SectionContainerUser } from "./style"
 import UserPhotoPost from "./UserPhotoPost/UserPhotoPost"
 import UserStats from "./UserStats/UserStats"
+import {UserContext} from '../../contexts/UserContext'
+import { useContext } from "react"
 import Feed from "../Feed/Feed"
 
 const User = () => {
+  const {dataUser} =  useContext(UserContext)
   return (
     <SectionContainerUser className="container" >
       <UserHeader/>
       <Routes>
-        <Route path="/"  element={<Feed/>}/>
+        <Route path="/"  element={<Feed user={dataUser.id}/>}/>
         <Route path="postar" element={<UserPhotoPost/>}/>
         <Route path="estatisticas" element={<UserStats/>}/>
       </Routes>
