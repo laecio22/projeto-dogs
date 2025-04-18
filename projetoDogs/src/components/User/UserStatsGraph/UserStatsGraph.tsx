dcde265012b2ff92273a6deca0cb45f2c6c865c4
laecio22 <laecon22@hotmail.com>
2025-04-18 15:47:02 -0300

    feat:conclusão  implementação  componente  de gráfico

--- projetoDogs/src/components/User/UserStatsGraph/UserStatsGraph.tsx
+++ projetoDogs/src/components/User/UserStatsGraph/UserStatsGraph.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import {VictoryBar, VictoryPie, VictoryChart} from 'victory'
+import { SectionGraph,GraphItem } from "./style";
 
 const UserStatsGraph = ({data}) => {
 
@@ -6,18 +8,42 @@ const UserStatsGraph = ({data}) => {
   const [graph, setGraph] = useState([]);
 
   useEffect(()=>{
-     console.log(data, 'data')
+    const graphData = data.map((item)=>{
+       return {
+        x:item.title,
+        y: Number(item.acessos)
+       }
+    })
+
      setTotal(data.map(({acessos})=> Number(acessos)).reduce((prev, prox)=> prev + prox, 0))
+     setGraph(graphData)
   },[data])
 
-  return <section>
-    <div>
+  return <SectionGraph>
+    <GraphItem className="total">
         <p>Acesso: {total}</p>
-    </div>
-    <div>
+    </GraphItem>
+    <GraphItem>
+       <VictoryPie data={graph} innerRadius={50} padding={{top:20, bottom:20, left:80, right:80}}
+       style={{
+         data: {
+           fillOpacity:0.9,
+           stroke:'#fff',
+           strokeWidth: 2,
+         },
 
-    </div>
-  </section>;
+         labels: {
+            fontSize:12,
+            color: '#333'
+         }
+       }}/>
+    </GraphItem>
+    <GraphItem>
+        <VictoryChart>
+            <VictoryBar data={graph} alignment="start" ></VictoryBar>
+        </VictoryChart>
+    </GraphItem>
+  </SectionGraph>;
 };
 
 export default UserStatsGraph;
---
+++ projetoDogs/src/components/User/UserStatsGraph/style.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const SectionGraph = styled.div`
+display: grid;
+grid-template-columns: 1fr 1fr;
+gap: 2rem;
+@media screen and (max-width:40rem) {
+     grid-template-columns: 1fr;
+     margin-bottom:2rem;
+    
+
+  }
+`
+
+export  const GraphItem =  styled.div`
+  box-shadow: 0 10px 20px rgba(0,0,0,0.1);
+  border-radius: 0.2rem ;
+  display: grid;
+  align-items:center;
+
+  &.total {
+    grid-column: 1/3;
+    padding: 2rem;
+    font-size: 2.5rem;
+    @media screen and (max-width:40rem) {
+    grid-column: 1;
+
+  }
+
+
+  }
+
+ 
+`
